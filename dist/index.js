df494e50898c49cf2c8e05e4db46c0cc95001b21
Wilfried Prombo <wilfriedprombo02@gmail.com>
2024-01-24 15:41:57 +0100

    :bulb: Adding comments on concepts learned: basic syntax, type narrowing, alias & generics

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,18 @@
 "use strict";
+// Basic syntax Narrowing type Alias & Generics
+// const hello: string = 'Hello world'
+// const words: string[] = ['Hello', 'World']
+// const bool: boolean = true
+// const numbers: number[] = [1, 2]
+// const obj: object = {firstProp: 'super obj', secondProp: 1}
+// const isNull: null = null
+// const undef: undefined = undefined
+// const button = document.querySelector('.button') as HTMLButtonElement
+// const button = <HTMLButtonElement>document.querySelector('.button')
+// function increment<NumType>(num: NumType): NumType {
+//   return num
+// }
+// const num = increment<number>(3)
 let count = 0;
 const btnIncrement = document.querySelector(".btn-increment");
 const viewScore = document.querySelector(".view");
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
+// Basic syntax Narrowing type Alias & Generics
+
+// const hello: string = 'Hello world'
+// const words: string[] = ['Hello', 'World']
+// const bool: boolean = true
+// const numbers: number[] = [1, 2]
+// const obj: object = {firstProp: 'super obj', secondProp: 1}
+// const isNull: null = null
+// const undef: undefined = undefined
+
+// const button = document.querySelector('.button') as HTMLButtonElement
+// const button = <HTMLButtonElement>document.querySelector('.button')
+
+// function increment<NumType>(num: NumType): NumType {
+//   return num
+// }
+
+// const num = increment<number>(3)
+
+
 let count: number = 0
 
 const btnIncrement = <HTMLButtonElement>document.querySelector(".btn-increment")
