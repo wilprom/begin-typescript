e3dbd236286ea411c1e893ad658032e6c27dce6b
Wilfried Prombo <wilfriedprombo02@gmail.com>
2024-01-25 19:05:03 +0100

    :bulb: Adding comments on concepts learned: Class, Type & Interface, Tuple & Enum

--- dist/index.js
+++ dist/index.js
@@ -22,3 +22,51 @@ const handleIncrement = (e) => {
     span.innerText = count.toString();
 };
 btnIncrement.addEventListener("click", handleIncrement);
+// Class typescript
+// abstract class Person {
+//   private firstName: string
+//   private age: number
+//   constructor(firstName: string, age: number) {
+//     this.firstName = firstName
+//     this.age = age
+//   }
+//   getFirstName() {
+//     return this.firstName
+//   }
+//   getAge() {
+//     return this.age
+//   }
+// }
+// class Student extends Person {
+//   private serialNumber: string
+//   constructor(firstName: string, age: number, serialNumber: string) {
+//     super(firstName, age)
+//     this.serialNumber = serialNumber
+//   }
+//   getSerialNumber() {
+//     return this.serialNumber
+//   }
+// }
+// const student1 = new Student('Wilfried', 22, '20S48237')
+// console.log(student1.getFirstName())
+// console.log(student1.getAge())
+// console.log(student1.getSerialNumber())
+// Type & Interface
+// type User = {
+//   firstName: string,
+//   lastName: string,
+//   age: number
+// } Can use primary types & Cannot be redefined
+// interface Geometry {
+//   x: number
+//   y: number
+// } Can be extended & Can be implemented by a class
+// Tuple & Enum
+// Fixed-size table
+// const twoDimensionArray: [string, number] = ["Hello", 1]
+// console.log(twoDimensionArray)
+// enum statut {
+//   pending = "pending",
+//   paid = "paid",
+// }
+// console.log(statut.pending)
